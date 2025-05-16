89a1ca97fe1013591b24448123e21f74773c95d4
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-05-17 02:37:05 +0300

    update file app.js

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const userRoutes = require('./routes/adminUser.routes');
-const productRoutes = require('./routes/adminProduct.routes');
-
+const db = require("./db/db");
 app.use(express.json());
 
-// مثال على middleware مصادقة تجريبي (لا تستخدمه في الإنتاج)
 app.use((req, res, next) => {
-  req.user = { id: 1, role: 'admin' }; // مثال فقط، استبدله بـ JWT auth
+  req.user = { id: 1, role: 'admin' }; 
   next();
 });
+// ================== task 1 user =====================
 
-// Routes
-app.use('/admin/users', userRoutes);
-app.use('/admin/products', productRoutes);
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
-// تشغيل السيرفر
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+// ================== task 2 user =====================
+
+const productRoutes = require('./routes/product');
+app.use('/api', productRoutes);
+
+// =================task 3 user =========================
+const cartRoutes = require("./routes/cart");
+const wishlistRoutes = require("./routes/wishlist");
+app.use("/cart", cartRoutes);
+app.use("/wishlist", wishlistRoutes);
+
+
+
+// ================== task 3 admin =====================
+const adminuserRoutes = require('./routes/adminUser.routes');
+app.use('/admin/users', adminuserRoutes);
+
+
+// ================== task 4 admin =====================
+const productadminroute = require('./routes/adminProduct.routes');
+app.use('/admin/products', productadminroute);
+
+// ================== run Server =========================
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 const cors = require('cors');
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('API is running ✅');
-});
+  res.send('API is running ✅');});
